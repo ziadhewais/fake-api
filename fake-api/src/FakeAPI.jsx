d2e006408425ac7889716416b41bf46b6e4d79a2
zewais <arch.ziad.ewais@gmail.com>
2024-02-14 22:31:33 -0500

    This is testing collabration

--- fake-api/src/FakeAPI.jsx
+++ fake-api/src/FakeAPI.jsx
@@ -1,24 +1,24 @@
 import { useState, useEffect } from "react";
 import FakeAPICardsContainer from "./FakeAPICardsContainer";
 
-export default function FakeAPI(){
-    const [todos, setTodos] = useState([])
+export default function FakeAPI() {
+  const [todos, setTodos] = useState([]);
 
-    useEffect(() => {
-        fetchTodos()
-    },[])
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
-    async function fetchTodos(){
-        const response = await fetch("http://jsonplaceholder.typicode.com/todos/")
-        const fetchedData = await response.json()
-        console.log(fetchedData)
-        setTodos(fetchedData)
-    }
+  async function fetchTodos() {
+    const response = await fetch("http://jsonplaceholder.typicode.com/todos/");
+    const fetchedData = await response.json();
+    console.log(fetchedData);
+    setTodos(fetchedData);
+    //we are adding some text to commit
+  }
 
-    return(
-        <div>
-            <FakeAPICardsContainer todos={todos}/>
-        </div>
-    )
-
+  return (
+    <div>
+      <FakeAPICardsContainer todos={todos} />
+    </div>
+  );
 }
